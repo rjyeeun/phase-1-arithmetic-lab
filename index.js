a1da26e2a4cb42264bf2897ba67b3d93cb8f55c0
Yeeun Jung <rjyeeun@gmail.com>
2022-12-12 17:11:52 -0500

    commit tree 1c54cf90671bc28bd817155eedd7d7c2837e989e

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+//Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
+
+//Create a variable called random that will generate a random integer greater than 0.
+
+const random = Math.random()
+console.log(random)
+//create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+
+//Create a variable called max that finds the highest number in a set; the value returned should be 20.
+
